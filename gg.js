a1b8265a9c6c7801b397b789f3850e685cb317e9
toyeuthman <uthmantoye@gmail.com>
2023-09-09 23:29:32 +0100

    commit

--- gg.js
+++
@@ -1,9 +0,0 @@
-rf = {
-  slack_name: "Toye Utman",
-  currentDay: "Saturday",
-  utc_time: ["2023-09-09T19:32:15", "754Z"],
-  track: "backend",
-  github_file_Url: "https://github.com/toyeuthman/",
-  github_repo_url: "https://github.com/toyeuthman/Task-1.git",
-  status_code: 200,
-};
